6684a12d4325f9dce33312e0edc7880a01bd34fe
Khushi <khushijukanti33@gmail.com>
2024-05-12 10:30:56 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MatrixGrid from './components/MatrixGrid';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MatrixGrid />
+    </div>
+  );
+}
+
+export default App;
